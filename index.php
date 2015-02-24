4b45f2dee3637fd9d8e41043d78198cc9c888677
Lorenz Merdian <lorenz.merdian@googlemail.com>
2015-02-24 09:56:32 +0100

    structure of install

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+include 'install.php';
 
-include 'install.php'
-
-
+// check for existing of configuration.json
+// check for correct json format of configuration.json
+// create "ready to go" button
+// perform db install
+// perform php class generation
 ?>
