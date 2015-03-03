a07b3271635213ecb24a57328248785d3c108342
Lorenz Merdian <lorenz.merdian@googlemail.com>
2015-03-03 07:30:22 +0100

    At least some sort of compiling PHP code should be there

--- index.php
+++ index.php
@@ -6,11 +6,16 @@ include 'Configuration.php';
         <title>Alcuin &mdash; DB and REST for the layy ones</title>
     </head>
     <body>
+        <?php
+        // check for existing of configuration.json
+        if (file_exists('configuration.json')) {
+            
+        }
+
+        ?>
     </body>
 
-// check for existing of configuration.json
-
-if (file_exists('configuration.json'))
+<?php
 // check for correct json format of configuration.json
 // create "ready to go" button
 // perform db install
