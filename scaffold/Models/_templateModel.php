96567cb141d83812618242fcbebd2255f3df35ec
Lorenz Merdian <lorenz.merdian@googlemail.com>
2015-03-08 00:36:56 +0100

    extended model

--- scaffold/Models/_templateModel.php
+++ scaffold/Models/_templateModel.php
@@ -1,7 +1,42 @@
 <?php
 
 class {{model_name}}Model extends AlcuinModel {
-
+    
+    private $_belongs_to = array();
+    private $_belongs_to_and_has_many = array();
+    private $has_many = array();
+    
+    public function get($id) {
+      if (isset($id)) {
+        // GET on One element
+      } else {
+        // LIST all elements
+      }
+    }
+    
+    public function post($id, $content) {
+        if (isset($id)) {
+            // POST on one elelment
+        } else {
+            
+        }
+    }
+    
+    public function put($id, $content) {
+        if (isset($id)) {
+            // POST on one elelment
+        } else {
+            
+        }
+    }
+    
+    public function delete($id, $content) {
+        if (isset($id)) {
+            // POST on one elelment
+        } else {
+            
+        }
+    }
 }
 
 ?>
