c3ed237be5fd0d411bed5af1ea69979d48597b24
Lorenz Merdian <lorenz.merdian@googlemail.com>
2015-03-09 16:38:35 +0100

    Update _templateModel.php

--- scaffold/Models/_templateModel.php
+++ scaffold/Models/_templateModel.php
@@ -6,36 +6,20 @@ class {{model_name}}Model extends AlcuinModel {
     private $_belongs_to_and_has_many = array();
     private $has_many = array();
     
-    public function get($id) {
-      if (isset($id)) {
-        // GET on One element
-      } else {
-        // LIST all elements
-      }
+    function __construct($id = null) {
+        
     }
     
-    public function post($id, $content) {
-        if (isset($id)) {
-            // POST on one elelment
-        } else {
-            
-        }
+    public function __set($key, $value) {
+        
     }
     
-    public function put($id, $content) {
-        if (isset($id)) {
-            // POST on one elelment
-        } else {
-            
-        }
+    public function save() {
+        // create if not ID
+        // update if id exists
     }
     
-    public function delete($id, $content) {
-        if (isset($id)) {
-            // POST on one elelment
-        } else {
-            
-        }
+    public function delete() {
     }
 }
 
