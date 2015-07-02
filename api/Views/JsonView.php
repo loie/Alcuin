19c4f8db80810ad20e69eee6586b1076c51671e3
loie <lorenz.merdian@googlemail.com>
2015-07-02 21:32:23 +0200

    improved content type for match those for the json api

--- api/Views/JsonView.php
+++ api/Views/JsonView.php
@@ -2,7 +2,7 @@
 
 class JsonView extends ApiView {
     public function render($content) {
-        header('Content-Type: application/json; charset=utf8');
+        header('Content-Type: application/vnd.api+json');
         echo json_encode($content);
         return true;
     }
